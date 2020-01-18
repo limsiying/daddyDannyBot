28aa96d372409682c40d9f36c8b1dfa56b61c39c
limsiying <siying1899@gmail.com>
2020-01-19 05:28:10 +0800

    working japanese chatbot

--- bot.py
+++ bot.py
@@ -11,24 +11,24 @@ DIALOG_TOKEN = '02ab3a2fe7494e2c80d38726ebeec2b0'
 updater = Updater(token=TOKEN, use_context=True)
 dp = updater.dispatcher
 
-LANGUAGE, MODE = range(2)
-
 def start(update, context):
-    reply_keyboard = [['Japanese']]
+    reply_keyboard = [['/japanese']]
     update.message.reply_text(
-            'Hi! This is Daddy Danny, your best friend and go-to AI for learning Japanese with!'
-            + ' Click to begin your journey to mastering 日本語 today. Daddy cannot wait'
-            + ' (✿ ◕‿◕)',
+            'Hi! This is Daddy Danny, your best friend and go-to AI for learning languages!'
+            + ' Please select which language you want to learn (✿ ◕‿◕)',
             reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    return LANGUAGE
 
+start_handler = CommandHandler('start', start)
+dp.add_handler(start_handler)
 
 def selectMode(update, context):
-    reply_keyboard = [['Alphabet', 'Vocabulary', 'Chat', 'Exit']]
+    reply_keyboard = [['/alphabet', '/vocabulary', '/chat', '/exit']]
     update.message.reply_text(
-            '°。+ *´¨What do you want to do today?°。+ *´¨',
+            '°。*´¨What do you want to do today?° *´¨',
             reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
-    return MODE
+
+mode_handler = CommandHandler('japanese', selectMode)
+dp.add_handler(mode_handler)
 
 def generateAlphabet(update, context):
     context.bot.send_message(chat_id=update.message.chat_id, text=' ひらがな')
@@ -42,38 +42,47 @@ dp.add_handler(CommandHandler('vocabulary', generateVocabulary))
 
 def translateJapaneseToEnglish(text):
     translator = Translator()
-    translator.translate(text, dest='en', src='auto')
-    return text
+    translation = translator.translate(text, dest='en', src='ja')
+    #print(translation.text)
+    return translation.text
 
-def receiveMessage(update, context):
+def translateEnglishToJapanese(text):
+    translator = Translator()  # initalize the Translator object
+    translation = translator.translate(text, dest='ja', src='en')  # translate two phrases to Hindi
+    #print(translation.text)  
+    return translation.text
+
+def chat(update, context):
     request = apiai.ApiAI(DIALOG_TOKEN).text_request()
-    request.lang = 'auto'
+    request.lang = 'en'
     request.session_id = 'Small-Talk'
-    request.query = update.message.text
-    responseJson = json.loads(request.getresponse().read().decode('utf-8'))
+    raw_message = update.message.text
+    message_en = translateJapaneseToEnglish(raw_message)
+    request.query = message_en # Send request to AI with user message
+    responseJson = json.loads(request.getresponse().read().decode('utf-8')) # response from server
     response = responseJson['result']['fulfillment']['speech'] # Take JSON answer, response is a string?    
     if response:
-        response = translateJapaneseToEnglish(response)
-        context.bot.send_message(chat_id=update.message.chat_id, text=response)
+        response_ja = translateEnglishToJapanese(response)
+        context.bot.send_message(chat_id=update.message.chat_id, text=response_ja)
     else:
-        context.bot.send_message(chat_id=update.message.chat_id, text='I dont understand!')
+        context.bot.send_message(chat_id=update.message.chat_id, text='くそ! パパ・ダニーはばかです...ご不便をお許しください o(╥﹏╥)o')
 
-dp.add_handler(MessageHandler(Filters.text, receiveMessage))
+dp.add_handler(MessageHandler(Filters.text, chat))
 
-def exit():
-    update.message.reply_text('((o(;△;)o)) Nuuu you have to go? Daddy is sad...COME BACK SOON Ó╭╮Ò')
-    return ConversationHandler.END
+def chat(update, context):
+    context.bot.send_chat_action(chat_id=update.effective_message.chat_id, action=ChatAction.TYPING)
+
+dp.add_handler(CommandHandler('chat', chat))
+
+def exit(update, context):
+    reply_keyboard = [['/start']]
+    update.message.reply_text('((o(;△;)o)) Nuuu you have to go? Daddy is sad...COME BACK SOON Ó╭╮Ò', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    threading.Thread(target=shutdown).start()
+
+exit_handler = CommandHandler('exit', exit)
+dp.add_handler(exit_handler)
 
 def main():
-    conv_handler = ConversationHandler(
-            entry_points=[CommandHandler('start', start)],
-            states={
-                LANGUAGE: [MessageHandler(Filters.regex('^(Japanese)$'), selectMode)],
-                MODE: [MessageHandler(Filters.regex('^(Alphabet|Vocabulary|Chat|Exit)$'), start)]
-                },
-            fallbacks=[CommandHandler('exit', exit)]
-            )
-    dp.add_handler(conv_handler)
     updater.start_polling(clean=True)
     updater.idle()
 
